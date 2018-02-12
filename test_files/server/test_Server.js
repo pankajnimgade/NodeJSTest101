65cc5222bc85f382b8c12522cc5bf4ea8ddb5557
Pankaj Nimgade <pankaj.nimgade@gmail.com>
2018-02-11 19:21:28 -0600

    added server file

---
+++ test_files/server/test_Server.js
@@ -0,0 +1,11 @@
+var http = require('http');
+
+function onRequest(request, response){
+    console.log("A user made a request"+request.url);
+    response.writeHead(200, {"Context-Type":"text/plain"});
+    response.write("here is some data");
+    response.end();
+}
+
+http.createServer(onRequest).listen(8080);
+console.log("Server is now running...");
